5a3c0696dc5c3d0562f8b241bb8fa452a04712ac
kpatenio <patenio@ualberta.ca>
2020-11-12 23:47:29 -0800

    feat: added react-testing-library + sass

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { getByText } from '@testing-library/react';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('render semantic ui button', () => {
+  const {container, getByText} = render(<App />);
+  expect(getByText("Click Me!")).toBeInTheDocument();
+})

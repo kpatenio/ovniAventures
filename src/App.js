a7b58efa9bacb0f2a43d1c8d6e8cf9fee7c379c1
kpatenio <25045015+kpatenio@users.noreply.github.com>
2020-11-13 00:21:53 -0800

    feat: added scss + react testing library + semantic ui (#3)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
-import './App.css';
+import 'semantic-ui-css/semantic.min.css';
+import './App.scss';
+import { Button } from 'semantic-ui-react';
 
 function App() {
   return (
@@ -17,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Button className="App-button">Click Me!</Button>
       </header>
     </div>
   );

fa99fb0ae62579df4af5653e5a38e00e9feb5774
kpatenio <25045015+kpatenio@users.noreply.github.com>
2020-11-19 04:55:30 -0800

    feat: updated landing page + revised for singleplayer only (at the moment) (#7)

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,18 +1,93 @@
 import React, { useState, useEffect } from 'react';
 import {
-    Button,
-    Container,
+    // Button,
+    // Container,
     Form,
     Header,
     Input,
     Modal
 } from 'semantic-ui-react';
 import {
+    withStyles,
+    makeStyles
+} from '@material-ui/core/styles';
+import {
+    Box,
+    Button,
+    Container,
+    Grid,
+    InputLabel,
+    Link,
+    TextField,
+    Typography
+} from '@material-ui/core';
+import {
+    sizing
+} from '@material-ui/system'
+import { green, purple, red } from '@material-ui/core/colors';
+import {
     useHistory
 } from 'react-router-dom';
 import NameModal from './modals/NameModal';
+import {
+    FONTS
+} from '../constants';
+import ovni from '../ovni-transparent.png';
+import earth from '../earth.png';
+
+const useStyles = makeStyles((theme) => ({
+    button: {
+        // border: "5px solid white",
+        fontFamily: FONTS.BODY,
+        fontWeight: "bold",
+        marginBotton: 10,
+        marginTop: 10
+    },
+    h1: {
+      fontFamily: FONTS.TITLE,
+      color: "white",
+    },
+    h2: {
+        fontFamily: FONTS.BODY,
+        color: "white",
+        marginBottom: 20
+    },
+    hr: {
+        marginTop: 30,
+        marginBottom: 30 
+    },
+    bodyText: {
+        fontFamily: FONTS.BODY,
+        color: "white"
+    },
+    image: {
+        height: 200,
+        width: 200,
+    },
+    earth: {
+        height: 200,
+        width: 200,
+    },
+    textfield: {
+        backgroundColor: "white",
+        // backgroundColor: "#1e1c21",
+        color: "black",
+        fontFamily: FONTS.BODY,
+        height: 40,
+        padding: 10
+    },
+    box: {
+        padding: 30,
+        // width: "50%"
+        // backgroundColor: "#352945",
+        // border: "2px solid black"
+    }
+}))
 
 function LandingPage() {
+
+    const classes = useStyles();
+
     const [isNameModalOpen, setIsNameModalOpen] = useState(false);
     const [playerName, setPlayerName] = useState('')
     const [isInvalidName, setIsInvalidName] = useState(true);
@@ -23,11 +98,6 @@ function LandingPage() {
         setIsNameModalOpen(true);
         setMode('solo');
         
-    }
-
-    const onClickMultiplayer = () => {
-        setIsNameModalOpen(true);
-        setMode('multijoueur');
     }
 
     const onClickCancelNameModal = () => {
@@ -68,15 +138,71 @@ function LandingPage() {
                     isInvalidName={isInvalidName}
                     onChangeInput={onChangeNameModalInput}
                 />
-                <Header inverted as="h1">Ovni Aventures</Header>
-                <Header inverted as="h2">Le jeu inspiré par le livre OVNI!</Header>
+                {/* <Header inverted as="h1">Ovni Aventures</Header> */}
+                <Grid direction="row" container justify="space-between" className={classes.h1}>
+                    <Grid item xs>
+                        <Typography variant="h1" className={classes.h1}>
+                            OVNI Aventures
+                        </Typography>
+                    </Grid>
+                    <Grid item xs>
+                        <img className={classes.image} src={ovni}/>
+                    </Grid>
+                </Grid>
+                {/* <Typography variant="h2" className={classes.h2}>Le jeu inspiré par le livre OVNI!</Typography> */}
+                <hr className={classes.hr}/>
                 <main>
-                    <Button onClick={onClickSolo} inverted>
-                        Jeu solo
+                    <Box className={classes.box}>
+                <Typography variant="h2" className={classes.h2}>
+                        Mode en solo
+                    </Typography>
+                    <Typography variant="body1" className={classes.bodyText} paragraph>
+                        Affrontez les conséquences d'un adversaire CPU! L'ovni prendra une décision au hasard, alors il faut être prêt.e à s'adapter pour survivre.
+                        </Typography>
+<InputLabel style={{color: "white", fontFamily: FONTS.BODY, marginBottom: 10}}>
+Saisir votre nom pour commencer:
+</InputLabel>
+                    <TextField variant="standard" fullWidth InputProps={{className: classes.textfield}}/>
+                    {/* <Typography variant="h2" className={classes.h2}>
+                        À propos
+                    </Typography> */}
+                    {/* <Typography variant="body1" className={classes.bodyText}>
+                        Jouer le jeu
+                    </Typography> */}
+                    <Button className={classes.button} onClick={onClickSolo} variant="contained" color="primary" size="large" fullWidth>
+                        Nouvelle partie
                     </Button>
-                    <Button onClick={onClickMultiplayer} inverted>
-                        Jeu multijoueur
-                    </Button>
+                    </Box>
+
+                    <Box className={classes.box}>
+                        <Typography variant="h2" className={classes.h2}>
+                            Les règles
+                        </Typography>
+                        <Typography variant="body1" className={classes.bodyText}>
+                        Assumer le rôle d'un personnage humain et affronter les conséquences de vos propres décisions... ainsi que celles d'autres! Essayez-vous de survivre jusqu'à la fin du jeu en faisant le bon choix pendant votre parcours.
+                        </Typography>
+                        <ul className={classes.bodyText}>
+                            <li>L'ovni et vous, vous jouerez à tour de rôle, et chaque décision affecte les décisions possibles pour chaque joueur.</li>
+                            <li>Vous commencez avec la santé pleine - un action nuisible la deminue. Si votre santé est toute épuisée, c'est « game over » pour vous!</li>
+                            <li>Si vous survivez la partie, vous gagnerez!</li>
+                        </ul>
+                    </Box>
+
+                    <Box className={classes.box}>
+                        <Typography variant="h2" className={classes.h2}>
+                            À propos
+                        </Typography>
+                        <Typography variant="body1" className={classes.bodyText}>
+                            <Box component="span" fontStyle="italic">Ovni aventures</Box> est inspiré par le livre <Box component="span" fontStyle="italic">OVNI</Box> des auteurs Fabrice Parme et Lewis Trondheim. Réalisé par <Link href="https://github.com/kpatenio/ovniAventures" underline="always" target="_blank" rel="noopener"> Katherine Patenio</Link>, le jeu est un recréation du livre original en format numérique et de la perspective d'un personnage humain.
+                                
+                        </Typography>
+                    </Box>
+
+                    <Grid container justify="center">
+                        <Grid item>
+                            <img className={classes.earth} src={earth}/>
+                        </Grid>
+                    </Grid>
                 </main>
             </Container>
         </>
---
+++ src/constants.js
@@ -0,0 +1,5 @@
+// Fonts use Google Fonts CDN defined in App css file
+export const FONTS = {
+    BODY: "Source Code Pro",
+    TITLE: "Stalinist One"
+}
